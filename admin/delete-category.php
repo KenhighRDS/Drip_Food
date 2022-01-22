6d6deaeff21c713c3c1e996a5c61be639027cfc5
KenhighRDS <wolfkennyy@gmail.com>
2022-01-21 23:42:52 -0500

    update

---
+++ admin/delete-category.php
@@ -0,0 +1,66 @@
+
+<?php
+    //Include constants file
+    include('../config/constants.php');
+
+
+    //echo "delete Page";
+    //check whether the id and image_name value is set or not
+    if(isset($_GET['id']) AND isset($_GET['image_name']))
+    {
+        //get the value and delete
+        //echo "Get value and delete";
+        $id = $_GET['id'];
+        $image_name = $_GET['image_name'];
+
+        //remove the physical image file is available
+        if($image_name != "")
+        {
+            //image is available. so removw it
+            $path = "../images/category/".$image_name;
+            //remove the image
+            $remove = unlink($path);
+
+            //if failed to remove image then add an error message and stop the process
+            if($remove==false)
+            {
+                //set the session message
+                $_SESSION['remove'] = "<div class='error'>Failed to remove Category Image.</div>";
+                //redirect to manage category page
+                header('location:'.SITEURL.'admin/manage-category.php');
+                //stop the process
+                die();
+            }
+        }
+
+        //delete data from database
+        //sql query to delete data from database
+        $sql = "DELETE FROM tbl_category WHERE id=$id";
+
+        //execute the query
+        $res = mysqli_query($conn, $sql);
+
+        //check whether the data is deleted from the database or not
+        if($res==true)
+        {
+            //set succes messge and redirect
+            $_SESSION['delete'] = "<div class='success'>Category Deleted Successfully.</div>";
+            //redirect to manage category
+            header('location:'.SITEURL.'admin/manage-category.php');
+        }
+        else
+        {
+            //set failed message and redirect
+            $_SESSION['delete'] = "<div class='error'>Failed to delete category.</div>";
+            //redirect to manage category
+            header('location:'.SITEURL.'admin/manage-category.php');
+        }
+
+        //redirect to manage category page with message
+    }
+    else
+    {
+        //redirect to manage category page
+        header('location:'.SITEURL.'admin/manage-category.php');
+    }
+?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,11 @@
+          
+          <!-- footer content section starts-->
+          <div class="footer">
+                <div class="wrapper">
+                   <p class="text-center">2021 All rights reserved, Bon Mamba. Developed By - <a href="#"> KenhighRDS</p>
+                </div> 
+          </div>  
+          <!-- footer section ends-->
+
+    </body>
+</html>
